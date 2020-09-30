b8880ff1e4ccb078a446a6d13e4f0ebb00e7af15
Moseyza <Moseyza@gmail.com>
2020-09-30 19:33:57 +0330

    changing theme of cartable selection menu

--- src/components/Cartable/CartableOwner/SingleCartableOwner.vue
+++ src/components/Cartable/CartableOwner/SingleCartableOwner.vue
@@ -1,33 +1,49 @@
 <template>
     <div style="display:flex">
-        <div v-if="letterOwners.length>1" style="margin-left:5px">
-            <select style="width: 20px; background-color: transparent;" v-model="selectedOwner" @change="onLetterOwnerChanged($event)">
+       
+      <div v-if="letterOwners.length>1 && isOwnersLoaded" style="margin-left:5px">
+            <div @click="activeDropdown()" id="cartables-dropdown"  class="ui icon top left dropdown">
+  		                <i class="action-icon icon-comboboxArrow" style="font-size: medium"></i>
+  		                <div class="left menu">
+    		                <div @click="onLetterOwnerChanged(owner.id)" v-for="owner in letterOwners" :key="owner.id" class="item menu-item"><div style="padding-left:5px">{{owner.nameOnly}}</div></div>
+  		                </div>
+	        </div>
+            <!-- <select style="width: 20px; background-color: transparent;" v-model="selectedOwner" @change="onLetterOwnerChanged($event)">
                 <option v-for="item in letterOwners" :key="item.id" :value="item.id">{{item.nameOnly}}</option>
-            </select>
+            </select> -->
         </div>
         <div>
-        {{ownerName}}
-        [{{post}}]
-      </div>
+            {{ownerName}}
+            [{{post}}]
+        </div>
+        
     </div>
 </template>
 
 <script lang="ts">
-import { Vue, Prop, Component } from 'vue-property-decorator';
+import { Vue, Prop, Component, Watch } from 'vue-property-decorator';
 import * as api from '@/store/api';
 import router from '@/router';
 import { LetterOwnerWithPicture } from '@/store/models/LetterOwner/letterOwnerWithPicture';
+import * as $ from 'jquery';
 
 @Component
 export default class SingleCartableOwner extends Vue{
     ownerName= '';
     post = '';
     ownerImage = '';
-    letterOwners = [] as LetterOwnerWithPicture[]
+    letterOwners = [] as LetterOwnerWithPicture[];
+    isOwnersLoaded = false;
+
+    @Watch("letterOwners")
+    onLetterOwnersChanged(n: LetterOwnerWithPicture[], o: LetterOwnerWithPicture[]){
+        //$(".dropdown").dropdown({action: 'hide',silent: true});
+        this.isOwnersLoaded = true;
+    }
     async created(){
         this.letterOwners = await  api.getLetterOwners();
         const currentLetterOwner = this.letterOwners[0];
-        this.selectedOwner = currentLetterOwner.id;
+        //this.selectedOwner = currentLetterOwner.id;
         this.post = currentLetterOwner.post;
         this.ownerName = currentLetterOwner.nameOnly;
         this.ownerImage = 'data:image/png;base64, ' + currentLetterOwner.picture;
@@ -35,11 +51,13 @@ export default class SingleCartableOwner extends Vue{
         this.$store.commit("setCanSeeAllLetters",currentLetterOwner.canSeeSecuredLetters);
         this.$emit('on-letter-owner-set',false);
         this.$router.replace("ReceivedLetters");
+        // $(".dropdown").dropdown({action: 'hide',silent: true});
+        
     }
-    selectedOwner = '';
-    onLetterOwnerChanged(e: any)
+    //selectedOwner = '';
+    onLetterOwnerChanged(ownerId: string)
     {
-        const currentOwner = this.letterOwners.find(x=>x.id === this.selectedOwner);
+        const currentOwner = this.letterOwners.find(x=>x.id === ownerId /*this.selectedOwner*/);
         if(!currentOwner)return;
         this.post = currentOwner.post;
         this.ownerName = currentOwner.nameOnly;
@@ -47,7 +65,16 @@ export default class SingleCartableOwner extends Vue{
         this.$store.commit("setOwnerId",currentOwner.id);
         this.$store.commit("setCanSeeAllLetters",currentOwner.canSeeSecuredLetters);
         this.$emit('on-letter-owner-set',true);
-        this.$router.push("ReceivedLetters");
+        //this.$router.push("ReceivedLetters");
+    }
+    mounted(){
+        $(".dropdown").dropdown({action: 'hide',silent: true});
+    }
+    isFirstClick = false;
+    activeDropdown(){
+        if(this.isFirstClick)return;
+        $(".dropdown").dropdown({action: 'hide',silent: true});
+        this.isFirstClick = true;
     }
 }
 </script>
